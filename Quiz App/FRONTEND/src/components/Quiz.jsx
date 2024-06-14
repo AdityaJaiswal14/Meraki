51134c12d981ac60df12c053f2668da09209f585
rushitjhaveri <rushitpjhaveri@gmail.com>
2024-06-14 14:10:02 +0530

    Modified quiz page

--- Quiz App/FRONTEND/src/components/Quiz.jsx
+++ Quiz App/FRONTEND/src/components/Quiz.jsx
@@ -30,7 +30,6 @@ export default function Quiz() {
 
   return (
     <div className='container'>
-        <h1 className='title text-light'>Quiz Application</h1>
 
         <Questions></Questions>
       
